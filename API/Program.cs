ce2327f788927aa436fcad752e7ca01b497e4bb4
TLAMHutto <keatonhutto8@gmail.com>
2022-10-13 03:51:40 -0500

    commt

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +10,20 @@ namespace API
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+          using var scope = host.Services.CreateScope();
+           var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+           var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+           try {
+                context.Database.Migrate();
+                DbInitializer.Initialize(context);
+              } catch (Exception ex) {
+                logger.LogError(ex, "An error occured during migration");
+              } 
+              host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
