29714c9c4496a9c6ceb2afdef1cf86a7274a2c3b
TLAMHutto <keatonhutto8@gmail.com>
2022-10-13 17:00:34 -0500

    comment

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -1,17 +1,41 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Data
 {
     public static class DbInitializer
     {
-        public static void Initialize(StoreContext context)
+        public static async Task Initialize(StoreContext context, UserManager<User> userManager)
         {
-            if (context.Products.Any()) return;
-            var Products = new List <Product>
+            if (!userManager.Users.Any())
             {
-                		new Product
+                var user = new User
+                {
+                    UserName = "bob",
+                    Email = "bob@test.com"
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
+
+                var admin = new User
+                {
+                    UserName = "admin",
+                    Email = "admin@test.com"
+                };
+
+                await userManager.CreateAsync(admin, "Pa$$w0rd");
+                await userManager.AddToRolesAsync(admin, new[] {"Member", "Admin"});
+            }
+
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
                 {
                     Name = "Angular Speedster Board 2000",
                     Description =
@@ -208,11 +232,12 @@ namespace API.Data
                     QuantityInStock = 100
                 },
             };
-            
-            foreach (var product in Products)
+
+            foreach (var product in products)
             {
                 context.Products.Add(product);
             }
+
             context.SaveChanges();
         }
     }
